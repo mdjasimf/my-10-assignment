ebb546a44fcdb0d1f533eb6916b7d8a133e8fecf
mdjasimf <mdjasimf@gmail.com>
2022-06-06 11:42:19 +0600

    jjj

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
-import Blogs from './components/Blogs/Blogs';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-import Login from './components/Login/Login';
 import Checkout from './components/Checkout/Checkout';
 import NotFound from './components/NOtFound/NotFound';
 import Registration from './components/Registration/Registration';
 import RequireAuth from './components/RequireAuth/RequireAuth';
+import Blogs from '../../my-11th-assignment-client/src/component/Blogs/Blogs';
 
 function App() {
   return (
@@ -19,7 +18,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/registration' element={<Registration></Registration>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/about' element={<About></About>}></Route>
